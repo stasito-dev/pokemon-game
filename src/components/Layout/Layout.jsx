8fcb51fa74c93f07b311be474a435723a8247534
Stanislav <yatskovstanislav2018@gmail.com>
2021-01-31 14:45:06 +0200

    homework-3

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,21 +1,22 @@
 import s from './Layout.module.css';
 
-const Layout = ({id, title, descr, urlBg = null, colorBg = null}) => {
+const Layout = ({id, title, urlBg = null, colorBg = null, colorTitle = null, children}) => {
     console.log(`#### id: urlBg: ${urlBg}`);
     console.log(`#### id: colorBg: ${colorBg}`);
     return (
         <section 
+
         className={s.root} id={id} 
         style={{backgroundImage: `url(${urlBg})`, 
         backgroundColor: `${colorBg}`}}>
             <div className={s.wrapper}>
                 <article>
                     <div className={s.title}>
-                        <h3>{title}</h3>
+                        <h3 style={{backgroundColor: `${colorTitle}`}}>{title}</h3>
                         <span className={s.separator}></span>
                     </div>
                     <div className={`${s.desc} ${s.full}`}>
-                        <p>{descr}</p>
+                        {children}
                     </div>
                 </article>
             </div>
